d2b2518e250b050a90d3bbf41ee229ca6a0a66de
Nguyễn Trung Hieu <78343616+trunghieu5520@users.noreply.github.com>
2021-05-31 18:11:17 +0700

    Update SpringBatchExample1ApplicationTests.java

--- spring-batch-example-1-master/src/test/java/com/techprimers/springbatchexample1/SpringBatchExample1ApplicationTests.java
+++ spring-batch-example-1-master/src/test/java/com/techprimers/springbatchexample1/SpringBatchExample1ApplicationTests.java
@@ -1,16 +1,58 @@
-package com.techprimers.springbatchexample1;
+package com.javatechie.crud.example;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.javatechie.crud.example.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import com.javatechie.crud.example.entity.Product;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@RunWith(SpringRunner.class)
+
+import java.util.List;
+
+
 @SpringBootTest
-public class SpringBatchExample1ApplicationTests {
+class SpringBootCrudExample2ApplicationTests {
+
+	@Autowired
+	ProductRepository repo;
+
 
 	@Test
-	public void contextLoads() {
+	public void testCreate(){
+		Product p = new Product();
+		p.setId(1);
+		p.setName("Head Phone");
+		p.setQuantity(1);
+		p.setPrice(10000);
+		repo.save(p);
+		assertNotNull(repo.findById(1).get());
+
 	}
 
+	@Test
+	public void testGetAll(){
+		List<Product> list = repo.findAll();
+		assertThat(list).size().isGreaterThan(0);
+	}
+
+	@Test
+	public void testUpdate(){
+		Product p = repo.findById(10).get();
+		p.setPrice(16000);
+		repo.save(p);
+		assertEquals(15000, repo.findById(10).get().getPrice());
+
+	}
+	@Test
+	public void testDelete () {
+		repo.deleteById(10);
+		assertThat(repo.existsById(10)).isFalse();
+	}
 }
