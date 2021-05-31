f83cc2ea0f8d9491ed65043e77ce7743a4bd6033
Nguyễn Trung Hieu <78343616+trunghieu5520@users.noreply.github.com>
2021-05-31 18:05:29 +0700

    Update SpringBootCrudExample2ApplicationTests.java

--- Spring Boot/spring-boot-crud-example-master/src/test/java/com/javatechie/crud/example/SpringBootCrudExample2ApplicationTests.java
+++ Spring Boot/spring-boot-crud-example-master/src/test/java/com/javatechie/crud/example/SpringBootCrudExample2ApplicationTests.java
@@ -1,13 +1,58 @@
 package com.javatechie.crud.example;
 
+import com.javatechie.crud.example.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.javatechie.crud.example.entity.Product;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+import java.util.List;
+
 
 @SpringBootTest
 class SpringBootCrudExample2ApplicationTests {
 
+	@Autowired
+	ProductRepository repo;
+
+
 	@Test
-	void contextLoads() {
+	public void testCreate(){
+		Product p = new Product();
+		p.setId(1);
+		p.setName("Head Phone");
+		p.setQuantity(1);
+		p.setPrice(10000);
+		repo.save(p);
+		assertNotNull(repo.findById(1).get());
+
 	}
 
+	@Test
+	public void testGetAll(){
+		List<Product> list = repo.findAll();
+		assertThat(list).size().isGreaterThan(0);
+	}
+
+	@Test
+	public void testUpdate(){
+		Product p = repo.findById(10).get();
+		p.setPrice(16000);
+		repo.save(p);
+		assertEquals(15000, repo.findById(10).get().getPrice());
+
+	}
+	@Test
+	public void testDelete () {
+		repo.deleteById(10);
+		assertThat(repo.existsById(10)).isFalse();
+	}
 }
